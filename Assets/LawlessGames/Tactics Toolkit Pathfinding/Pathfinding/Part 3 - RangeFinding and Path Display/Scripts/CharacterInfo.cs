aef1ee63eb8120cc0bf27f1ede311774fdfe98d2
Raul Thunderbird Nascimento <hhthunderbird@gmail.com>
2023-11-04 23:10:24 -0400

    bla bla bla

---
+++ Assets/LawlessGames/Tactics Toolkit Pathfinding/Pathfinding/Part 3 - RangeFinding and Path Display/Scripts/CharacterInfo.cs
@@ -0,0 +1,153 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CharacterType
+{
+    Player, Enemy
+}
+public enum Status
+{
+    Outside,
+    Moving,
+    Awaiting
+}
+
+public enum CharacterAction
+{
+    Spawn,
+    Move,
+    Attack
+}
+
+public class CharacterMove
+{
+    public CharacterInfo Character;
+    public CharacterType Type;
+    public CharacterAction Action;
+}
+
+public class CharacterInfo : MonoBehaviour
+{
+    private OverlayTile _standingOnTile;
+    public OverlayTile StandingOnTile
+    {
+        get => _standingOnTile;
+        set => _standingOnTile = value;
+    }
+    
+    [SerializeField]
+    protected CharacterType _type;
+    
+    [SerializeField]
+    protected Status Status;
+
+    protected Vector3 _weaponSignPos;
+
+    protected bool _isSpawned;
+
+    protected Weapon _weapon;
+
+    [SerializeField]
+    private int _lifeLeft = 100;
+
+    [SerializeField]
+    private int _damage = 100;
+
+    [SerializeField]
+    protected float _speed;
+
+    [SerializeField]
+    protected int _movementRange;
+
+    [SerializeField]
+    protected List<OverlayTile> _path;
+
+    protected PathFinder _pathFinder;
+
+    protected RangeFinder _rangeFinder;
+
+    [SerializeField]
+    protected List<OverlayTile> _rangeFinderTiles;
+
+    protected SpriteRenderer _spriteRenderer;
+
+    [SerializeField] protected GameObject _rockPrefab;
+    [SerializeField] protected GameObject _paperPrefab;
+    [SerializeField] protected GameObject _scissorsPrefab;
+
+    public delegate void CharacterActionEvent( CharacterMove action );
+
+    public static event CharacterActionEvent CharacterActed;
+
+    protected void OnCharacterActed( CharacterMove action )
+    {
+        CharacterActed?.Invoke( action );
+    }
+
+    protected virtual void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        switch(_type)
+        {
+            case CharacterType.Player:
+                _spriteRenderer.sortingOrder = 0;
+                break;
+            case CharacterType.Enemy:
+                _spriteRenderer.sortingOrder = 3;
+                break;
+        }
+        Status = Status.Outside;
+        _pathFinder = new PathFinder();
+        _rangeFinder = new RangeFinder();
+        _rangeFinderTiles = new List<OverlayTile>();
+        _path = new List<OverlayTile>();
+        //_path.Capacity = _movementRange;
+    }
+
+    protected void PositionCharacterOnTile( OverlayTile tile )
+    {
+        StandingOnTile = tile;
+        transform.position = new Vector3( tile.transform.position.x, tile.transform.position.y + 0.0001f, tile.transform.position.z );
+        //GetComponent<SpriteRenderer>().sortingOrder = tile.GetComponent<SpriteRenderer>().sortingOrder;
+        GetComponent<SpriteRenderer>().sortingOrder = 3;
+
+        if(!_isSpawned)
+        {
+            _isSpawned = true;
+            OnCharacterActed( new CharacterMove()
+            {
+                Action = CharacterAction.Spawn,
+                Character = this,
+                Type = _type
+            }
+            );
+            Status = Status.Awaiting;
+            GetInRangeTiles();
+        }
+    }
+
+    protected void GetInRangeTiles()
+    {
+        switch(_type)
+        {
+            case CharacterType.Player:
+            _rangeFinderTiles = _rangeFinder.GetTilesInRange( new Vector2Int( StandingOnTile.gridLocation.x, StandingOnTile.gridLocation.y ), _movementRange );
+                break;
+            case CharacterType.Enemy:
+            _rangeFinderTiles = _rangeFinder.GetTilesInRange( new Vector2Int( StandingOnTile.gridLocation.x, StandingOnTile.gridLocation.y ), 10 );
+                break;
+        }
+
+        foreach(var item in _rangeFinderTiles)
+        {
+            item.ShowTile();
+        }
+    }
+
+    protected virtual void OnDestroy()
+    {
+        _spriteRenderer.sortingOrder = 0;
+    }
+}
+
---
+++ Assets/LawlessGames/Tactics Toolkit Pathfinding/Pathfinding/Part 3 - RangeFinding and Path Display/Scripts/MouseController.cs
@@ -0,0 +1,161 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class MouseController : CharacterInfo
+{
+    
+    
+    private GameObject _weaponSign;
+
+    public GameObject cursor;
+
+    
+    private ArrowTranslator arrowTranslator;
+    
+    private bool _isMoving;
+    private Vector2Int _lastMove;
+
+    private void Start()
+    {
+        _weapon = (Weapon)Random.Range( 0, 3 );
+        _weaponSignPos = transform.GetChild( 0 ).localPosition;
+        MapManager.Instance.SetHelperPattern( _weapon );
+
+        arrowTranslator = new ArrowTranslator();
+
+        
+        _isMoving = false;
+        
+    }
+
+    void LateUpdate()
+    {
+        RaycastHit2D? hit = GetFocusedOnTile();
+
+        if(hit.HasValue)
+        {
+            Status = Status.Moving;
+
+            OverlayTile tile = hit.Value.collider.gameObject.GetComponent<OverlayTile>();
+            cursor.transform.position = tile.transform.position;
+            cursor.gameObject.GetComponent<SpriteRenderer>().sortingOrder = tile.transform.GetComponent<SpriteRenderer>().sortingOrder;
+
+            if(_rangeFinderTiles.Contains( tile ) && !_isMoving)
+            {
+                _path = _pathFinder.FindPath( StandingOnTile, tile, _rangeFinderTiles );
+
+                foreach(var item in _rangeFinderTiles)
+                {
+                    MapManager.Instance.Map[ item.grid2DLocation ].SetSprite( ArrowDirection.None );
+                }
+
+                for(int i = 0; i < _path.Count; i++)
+                {
+                    var previousTile = i > 0 ? _path[ i - 1 ] : StandingOnTile;
+                    var futureTile = i < _path.Count - 1 ? _path[ i + 1 ] : null;
+
+                    var arrow = arrowTranslator.TranslateDirection( previousTile, _path[ i ], futureTile );
+                    _path[ i ].SetSprite( arrow );
+                }
+            }
+
+            if(Input.GetMouseButtonDown( 0 ))
+            {
+
+                if(tile.Previous != null)
+                {
+                    _lastMove = tile.grid2DLocation - tile.Previous.grid2DLocation;
+                }
+
+
+                tile.ShowTile();
+
+                if(!_isSpawned)
+                {
+                    PositionCharacterOnTile( tile );
+                    _spriteRenderer.sortingOrder = 3;
+                    GetInRangeTiles();
+                    SetSign();
+                }
+                else
+                {
+                    _isMoving = true;
+                    tile.gameObject.GetComponent<OverlayTile>().HideTile();
+                }
+            }
+        }
+
+        if(_path.Count > 0 && _isMoving)
+        {
+            MoveAlongPath();
+        }
+    }
+
+    private void MoveAlongPath()
+    {
+        var step = _speed * Time.deltaTime;
+
+        float zIndex = _path[ 0 ].transform.position.z;
+        transform.position = Vector2.MoveTowards( transform.position, _path[ 0 ].transform.position, step );
+        transform.position = new Vector3( transform.position.x, transform.position.y, zIndex );
+
+        if(Vector2.Distance( transform.position, _path[ 0 ].transform.position ) < 0.00001f)
+        {
+            PositionCharacterOnTile( _path[ 0 ] );
+            _path.RemoveAt( 0 );
+        }
+
+        if(_path.Count == 0)
+        {
+            GetInRangeTiles();
+            _isMoving = false;
+
+            _weapon = MapManager.Instance.ProcessMovement( _weapon, _lastMove, out Sprite sprite );
+            SetSign();
+
+            OnCharacterActed( new CharacterMove() {
+                Action = CharacterAction.Move,
+                Character = this,
+                Type = _type
+            } );
+        }
+    }
+
+    private void SetSign()
+    {
+        if(_weaponSign)
+            Destroy( _weaponSign );
+
+        switch(_weapon)
+        {
+            case Weapon.ROCK:
+                _weaponSign = Instantiate( _rockPrefab, transform );
+                break;
+            case Weapon.PAPER:
+                _weaponSign = Instantiate( _paperPrefab, transform );
+                break;
+            case Weapon.SCISSORS:
+                _weaponSign = Instantiate( _scissorsPrefab, transform );
+                break;
+        }
+        _weaponSign.transform.localPosition = _weaponSignPos;
+    }
+
+    
+
+    private static RaycastHit2D? GetFocusedOnTile()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint( Input.mousePosition );
+        Vector2 mousePos2D = new Vector2( mousePos.x, mousePos.y );
+
+        RaycastHit2D[] hits = Physics2D.RaycastAll( mousePos2D, Vector2.zero );
+
+        if(hits.Length > 0)
+        {
+            return hits.OrderByDescending( i => i.collider.transform.position.z ).First();
+        }
+
+        return null;
+    }
+}
